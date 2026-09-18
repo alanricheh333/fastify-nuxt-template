2c237186906e8941c261ad014074dc524e91f195
Alan Alrechah <43235105+alanricheh333@users.noreply.github.com>
2026-09-18 17:47:32 +0200

    feat: add Nuxt application shell

---
+++ apps/web/app/app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { locale, localeProperties } = useI18n()
+
+useHead(() => ({
+  htmlAttrs: {
+    lang: localeProperties.value.language ?? locale.value,
+    dir: localeProperties.value.dir ?? 'ltr',
+  },
+}))
+</script>
+
+<template>
+  <UApp>
+    <NuxtPwaManifest />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
